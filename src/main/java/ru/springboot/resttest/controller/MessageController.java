8a105abe28abc2b7c764d3e1e54e4c20f19817bd
AlekseiFalshtynskii <aleksei.falshtynskii@gmail.com>
2018-12-07 16:06:51 +0300

    random send error

--- src/main/java/ru/springboot/resttest/controller/MessageController.java
+++ src/main/java/ru/springboot/resttest/controller/MessageController.java
@@ -10,24 +10,32 @@ import ru.springboot.resttest.model.JmsMessage;
 import ru.springboot.resttest.service.JmsMessageService;
 import ru.springboot.resttest.service.JmsService;
 
+import java.util.Random;
+
 @RestController
 public class MessageController {
     private static final Logger LOG = LoggerFactory.getLogger(MessageController.class);
     private final JmsService jmsService;
     private final JmsMessageService jmsMessageService;
+    private final Random random;
 
     public MessageController(JmsService jmsService, JmsMessageService jmsMessageService) {
         this.jmsService = jmsService;
         this.jmsMessageService = jmsMessageService;
+        this.random = new Random();
     }
 
     @PostMapping(value = "/send")
     public void sendMessage(@RequestBody JmsMessage jmsMessage) {
         try {
+            if (this.random.nextBoolean()) {
+                throw new org.springframework.jms.IllegalStateException(new javax.jms.IllegalStateException("Send error"));
+            }
             this.jmsService.sendMessage(jmsMessage.getMessage());
             String receiveMessage = this.jmsService.receiveMessage("type = '" + jmsMessage.getMessage() + "'");
             jmsMessage.setProcessed(jmsMessage.getMessage().equals(receiveMessage));
         } catch (JmsException e) {
+            LOG.error(e.getMessage());
             jmsMessage.setProcessed(false);
         } finally {
             JmsMessage newMessage = this.jmsMessageService.save(jmsMessage);
